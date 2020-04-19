a928050b682b5070f371c73269e126f7ce0fd3f5
Darian Nwankwo <nwankwodarian@gmail.com>
2020-04-19 03:11:36 -0400

    cleaning up main file

--- chess.py
+++ chess.py
@@ -7,19 +7,6 @@ from tile import Tile
 from piece import Piece
 
 
-def get_mouse_click(window):
-    point = window.getMouse()
-    return point.getX(), point.getY()
-
-
-def get_tile(board, x, y):
-    x_offset, y_offset = PADDING // 2, PADDING // 2
-    xprime, yprime = x - x_offset, y - y_offset
-    col_ndx = chr( ceil(xprime / CHESS_PIECE_DIMENSIONS[1]) + ORDINAL_OFFSET)
-    row_ndx = ceil(yprime / CHESS_PIECE_DIMENSIONS[0])
-    return f"{col_ndx}{row_ndx}"
-
-
 def should_update_current_and_previous_tile(prev_tile, cur_tile):
     return cur_tile and prev_tile and (cur_tile != prev_tile) and prev_tile.still_active()
 
